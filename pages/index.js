f38276f497bd6deb38ae3a47d65dd1d8338b47c5
jason <js@jason.com>
2019-09-16 13:30:37 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const WelcomePage = () => (
+  <React.Fragment>
+    <h1 className="text-center mt-5">Welcome to Next.js</h1>
+    <img src="/static/mdb-react-small.png" alt="MDBReact" className="img-fluid d-block mx-auto mt-3" />
+  </React.Fragment>
+);
+
+export default WelcomePage;
